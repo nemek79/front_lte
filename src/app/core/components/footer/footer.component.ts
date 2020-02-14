ecd67a0d72b34dd865e36187b1fead36697e2a7b
git <davidalonso79@gmail.com>
2020-02-14 18:49:53 +0100

    retoques versión base

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  public version = '0.0.1';
+  public year = '2020';
+  public url = 'http://vir2al.es';
+  public labelUrl = 'Vir2aL';
+
   constructor() { }
 
   ngOnInit() {
